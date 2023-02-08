cdeb4bdc90c1245b5ea5957086ac6f75ce65baeb
lukecos <lukecose7483@gmail.com>
2023-02-08 19:33:21 +1100

    Further updates to homepage - underlines and colours

---
+++ index.js
@@ -0,0 +1,11 @@
+
+  const hoverElement = document.querySelector(".hover-element");
+  const hoverText = document.querySelector(".hover-text");
+  
+  hoverElement.addEventListener("mouseenter", function() {
+    hoverText.style.display = "block";
+  });
+  
+  hoverElement.addEventListener("mouseleave", function() {
+    hoverText.style.display = "block";
+  });
